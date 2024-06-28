a1c8417a88d3c015a5b9614838ded729a6342f2e
saffwon <saffwon786@gmail.com>
2024-06-28 15:13:57 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,63 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							Presidency College <br> Kempapura,Bengaluru-024
+						</div>
+				
+						<div class="footerText">
+							E-mail: admin@gmail.com
+						</div>
+						
+					</div>
+					
+					
+					<div class="col-sm-4">
+					</div>
+				
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								facebook.com/
+							</div>
+						
+							<div class="gp">
+								www.google.com/
+							</div>
+						
+							<div class="tw">
+								twitter.com/
+							</div>
+						
+							<div class="in">
+								linkedin.com/
+							</div>
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Copyright &copy; 2023 My-tour
+						</div>
+					</div>
+					</div>
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
